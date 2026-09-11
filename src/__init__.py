b9a3f9774e12ad91d6b5dd4688378dc3597fc684
Mesa ("Sn1ckers") <phentitmesa06@gmail.com>
2026-09-11 10:19:28 +0700

    chore: Version 1.3

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 # A beginner-friendly password manager with modern UI
 
 # Version constant - displayed in the application
-VERSION = "1.15"
+VERSION = "1.3"
 
 # Application name
 APP_NAME = "Simple Password Manager"
